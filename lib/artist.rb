59863e8b15d912d42ea6574aa14f7324efda057e
Sara Walker <sarawall1187@gmail.com>
2019-01-15 21:25:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,6 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@artists
   end
@@ -29,7 +25,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
