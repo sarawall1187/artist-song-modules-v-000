8ecf62660f21f1df7c48911e9a0dba3dbc633270
Sara Walker <sarawall1187@gmail.com>
2019-01-15 21:40:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,9 @@ class Song
   include Memorable::InstanceMethods, Paramable
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
